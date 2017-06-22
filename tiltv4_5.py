f9a4b9031dee18dacf0b1eb68ad1782b78164cdd
Blais <rolandblais@gmail.com>
2017-06-21 17:00:29 -0700

    Update tiltv4_5.py

--- tiltv4_5.py
+++ tiltv4_5.py
@@ -151,21 +151,17 @@ def main():
 
                                 #Brewometer Cloud Data
                                 #Change this to the address of your google sheet script
-                                r = requests.post('https://script.google.com/macros/s/AKfycby4CTRc7j2zmnl9zzd6FuFNV0VfCsT9-DkJmlwZ1uJg7PWW-z8/exec', data)
-                                #https://script.google.com/macros/s/AKfycby4CTRc7j2zmnl9zzd6FuFNV0VfCsT9-DkJmlwZ1uJg7PWW-z8/exec
+                                r = requests.post('https://Change.this.to.the.URL.of.your.google.sheet.script/exec', data)
                                 print r.text
-
 
                                 #Fermenator Cloud v3
                                 #Change this to the address of your google sheet script
-                                r = requests.post('https://script.google.com/macros/s/AKfycbxqflsg9YHb8UruGF7s3Qb7tOW6ypyl3KFiDuPlDQZfyoIKnNM/exec', data)
-                                #https://script.google.com/macros/s/AKfycbxqflsg9YHb8UruGF7s3Qb7tOW6ypyl3KFiDuPlDQZfyoIKnNM/exec
+                                r = requests.post('https://Change.this.to.the.URL.of.your.google.sheet.script/exec', data)
                                 print r.text
 
                                 #brewstat.us
                                 #Change this to the address of your google sheet script
-                                r = requests.post('https://www.brewstat.us/tilt/vw8maenmpK/log', data)
-                                #https://www.brewstat.us/tilt/vw8maenmpK/log#Change this to the address of your google sheet script
+                                r = requests.post('https://Change.this.to.the.URL.of.your.brewstat.us.log/exec', data)
                                 print r.text
                                 updateTime = updateTime + updateSecs
                                 nextUpdate = str(int(round(updateTime - time.time())))
